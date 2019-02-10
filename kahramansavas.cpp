3b8f1996b6341e7d9afaf6314276828044250975
sheldorion <oguzhanonal52@hotmail.com>
2019-02-10 05:49:49 +0200

    Update kahramansavas.cpp

--- kahramansavas.cpp
+++ kahramansavas.cpp
@@ -1,82 +1,85 @@
-#include < iostream > 
-#include < string > 
-#include < vector > 
-#include < ctime > 
-#include < numeric > 
-#include < math.h >
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <numeric>
+#include <math.h>
 
-  using namespace std;
+using namespace std;
 
 class Warrior {
-  public:
+public:
     string name;
-  int health, block, power;
-  int maxattack;
-  int maxblock;
+    int health, block, power;
+    int maxattack;
+    int maxblock;
 
-  Warrior(string name, int health, int block, int power) {
-    this - > name = name;
-    this - > health = health;
-    this - > block = block;
-    this - > power = power;
+    Warrior(string name, int health, int block, int power) {
+        this->name = name;
+        this->health = health;
+        this->block = block;
+        this->power = power;
 
-  }
+    }
 
-  int Attack() {
-    maxattack = fabs(rand() % power);
-    return maxattack;
-  }
+    int Attack() {
+        maxattack = fabs(rand() % power);
+        return maxattack;
+    }
 
-  int Block() {
-    maxblock = fabs(rand() % block);
-    return maxblock;
-  }
+    int Block() {
+        maxblock = fabs(rand() % block);
+        return maxblock;
+    }
+
 
 };
 
 class Battle {
-  public:
-    static string startafight(Warrior & a, Warrior & b) {
-      while (a.health > 0 && b.health > 0) {
-        Result(a, b);
-        if (b.health <= 0) {
-          b.health = 0;
-          cout << a.name << "Kazandi" << endl;
-          break;
-        }
-        Result(b, a);
-        if (a.health <= 0) {
-          a.health = 0;
-          cout << b.name << "Kazandi" << endl;
-          break;
+public:
+    static string startafight(Warrior &a, Warrior &b) {
+        while (a.health > 0 && b.health > 0) {
+            Result(a, b);
+            if (b.health <= 0) {
+                b.health = 0;
+                cout << a.name << "Kazandi" << endl;
+                break;
+            }
+            Result(b, a);
+            if (a.health <= 0) {
+                a.health = 0;
+                cout << b.name << "Kazandi" << endl;
+                break;
+            }
+
         }
 
-      }
-
-      return "Oyun bitti";
+        return "Oyun bitti";
 
     }
 
-  static void Result(Warrior & a, Warrior & b) {
-    int attackamount;
-    int blockamount;
+    static void Result(Warrior &a, Warrior &b) {
+        int attackamount;
+        int blockamount;
 
-    attackamount = a.Attack() - b.Block();
-    if (attackamount <= 0) {
-      attackamount = 1;
+        attackamount = a.Attack() - b.Block();
+        if (attackamount <= 0) {
+            attackamount = 1;
+        }
+
+        b.health = b.health - attackamount;
+        cout << a.name << " Saldirdi " << b.name << " verdigi zarar "
+             <<
+             attackamount << b.name << " kalan can " << b.health << endl;
     }
-
-    b.health = b.health - attackamount;
-    cout << a.name << " Saldirdi " << b.name << " verdigi zarar " <<
-      attackamount << b.name << " kalan can " << b.health << endl;
-  }
 };
 
 int main() {
-  srand(time(NULL));
-  Warrior thor("thor", 100, 50, 50);
-  Warrior hulk("hulk", 100, 60, 70);
-  Battle::startafight(hulk, thor);
+    srand(time(NULL));
+    Warrior thor("thor", 100, 50, 50);
+    Warrior hulk("hulk", 100, 60, 70);
+    Battle::startafight(hulk, thor);
 
-  return 0;
+
+    return 0;
 }
